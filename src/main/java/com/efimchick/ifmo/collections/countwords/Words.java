9190e91825f2f5808c212571d7cf91569bcc7f6b
thejerome <thejerome@mail.ru>
2020-04-15 17:57:34 +0300

    Collections Count Words initial

---
+++ src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
@@ -0,0 +1,12 @@
+package com.efimchick.ifmo.collections.countwords;
+
+
+import java.util.List;
+
+public class Words {
+
+    public String countWords(List<String> lines) {
+
+        return null;
+    }
+}
